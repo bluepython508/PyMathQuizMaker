b6216d238a434da0dcd0445a09897562a46379fc
bluepython508 <bluepython508@users.noreply.github.com>
2017-06-14 16:05:46 +0000

    Added reading of files/args for Qs in create()

--- QuizMaker.py
+++ QuizMaker.py
@@ -8,6 +8,15 @@ def take(args):
 	pass
 
 def create(args):
+	if args.inf:
+		f = open(args.inf,'r')
+		quizformat = f.read()
+		f.close()
+	elif args.string:
+		quizformat = args.string
+	elif args.stdin:
+		quizformat = sys.stdin.read()
+	
 	pass
 
 
@@ -19,7 +28,7 @@ def main():
 	parsecreate = subparse.add_parser('create')
 	parsetake = subparse.add_parser('take')
 	form = parsecreate.add_mutually_exclusive_group(required=True)
-	form.add_argument('-f', '--file', type=argparse.FileType('r'))
+	form.add_argument('-f', '--file', type=argparse.FileType('r'), dest=inf)
 	form.add_argument('-s', '--string')
 	form.add_argument('-r', '--stdin', action="store_true")
 	parsecreate.add_argument("outquiz_file", type=argparse.FileType('w'), default=sys.stdout)
