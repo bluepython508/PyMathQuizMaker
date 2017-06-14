9f91e1adb2f494a3d18057ee545d3a81e9219207
bluepython508 <bluepython508@users.noreply.github.com>
2017-06-14 15:56:21 +0000

    Added base method stubs and argument parser.

--- QuizMaker.py
+++ QuizMaker.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import argparse
+import os
+import sys
+
+def take(args):
+	pass
+
+def create(args):
+	pass
+
+
 
 
 def main():
-	
-
+	rootparse = argparse.ArgumentParser()
+	subparse = rootparse.add_subparsers()
+	parsecreate = subparse.add_parser('create')
+	parsetake = subparse.add_parser('take')
+	form = parsecreate.add_mutually_exclusive_group(required=True)
+	form.add_argument('-f', '--file', type=argparse.FileType('r'))
+	form.add_argument('-s', '--string')
+	form.add_argument('-r', '--stdin', action="store_true")
+	parsecreate.add_argument("outquiz_file", type=argparse.FileType('w'), default=sys.stdout)
+	parsecreate.set_defaults(mode=create)
+	parsetake.set_defaults(mode=take)
+	args = rootparse.parse_args()
+	args.mode(args)
 if __name__ == '__main__':
 	main()
 
