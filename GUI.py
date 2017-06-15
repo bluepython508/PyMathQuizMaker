2ab4201d12df284eee44f9122d3c4bc54c68be2d
bluepython508 <bluepython508@users.noreply.github.com>
2017-06-15 11:10:02 +0000

    Tried to fix icon issues. Failed. Removed icon from program. TKinter threw strange errors when I tried to use an icon. Removed icon.

--- GUI.py
+++ GUI.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from tkinter import *
+from Tkinter import *
 import turtle
 import pyautogui
+import os
 
 def mainwindow():
 	tk = Tk()
 	tk.title("Math Quiz Maker")
 	tk.geometry("500x500")
-	tk.wm_iconbitmap('favicon.ico')
-	
+	#imgicon = PhotoImage(file=os.path.join('/home/pi/Desktop/PyMathQuiz','icon.gif'))
+	#tk.tk.call('wm', 'iconphoto', root._w, imgicon)
+	text = Label(tk, text="Quizzes!")
+	text.pack()
+	return tk
 
 def main():
-	
+	tk = mainwindow()
+	tk.mainloop()
 
 if __name__ == '__main__':
 	main()
