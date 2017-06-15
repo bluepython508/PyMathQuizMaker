388ffd050fb750dac5d8c5a70ef42e153cc368d3
bluepython508 <bluepython508@users.noreply.github.com>
2017-06-15 10:51:47 +0000

    Created GUI window and prettifyed it.

--- GUI.py
+++ GUI.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from tkinter import *
+import turtle
+import pyautogui
 
-
+def mainwindow():
+	tk = Tk()
+	tk.title("Math Quiz Maker")
+	tk.geometry("500x500")
+	tk.wm_iconbitmap('favicon.ico')
+	
 
 def main():
 	
